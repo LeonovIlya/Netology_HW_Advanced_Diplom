7de6e82393f14d9de09f68f9427954f3bef3aa2d
LeonovIlya <zlayapisya@gmail.com>
2022-04-03 19:43:11 +0300

    second version

--- main.py
+++ main.py
@@ -1,27 +1,86 @@
-from random import randrange
+import time
+from db import db
+import vk
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
-import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
-
-token = input('Token: ')
-
-vk = vk_api.VkApi(token=token)
-longpoll = VkLongPoll(vk)
-
-
-def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+my_login = "user_login"
+my_password = "user_password"
+token_group = "group_token"
 
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
+class VkGroup:
+    def __init__(self, login, password, token):
+        self.login = login
+        self.password = password
+        self.token_group = token
+        self.receive_message = vk.Communication(self.token_group).listen()
+        self.mess_id = self.receive_message[0]
+        self.sending_message = vk.Communication(self.token_group)
+        self.admin_group = vk.User(self.login, self.password)
 
-        if event.to_me:
-            request = event.text
+    def mess_text(self):
+        receive_message = vk.Communication(self.token_group).listen()
+        return receive_message[1]
 
-            if request == "привет":
-                write_msg(event.user_id, f"Хай, {event.user_id}")
-            elif request == "пока":
-                write_msg(event.user_id, "Пока((")
-            else:
-                write_msg(event.user_id, "Не поняла вашего ответа...")
+    def data_checking(self):
+        user_vk = self.admin_group.users_id(self.mess_id)
+        search_term = self.admin_group.requirements(user_vk)
+        if search_term["age_from"] is None and search_term["age_to"] is None:
+            self.sending_message.send_message(self.mess_id, f"Не хватает данных для поиска")
+            self.sending_message.send_message(self.mess_id, f"Введите свой возраст")
+            receive_message = vk.Communication(token_group).listen()
+            search_term["age_from"] = int(receive_message[1]) - 2
+            search_term["age_to"] = int(receive_message[1]) + 2
+        if search_term["sex"] is None:
+            self.sending_message.send_message(self.mess_id, f"Укажите пол для поиска")
+            self.sending_message.send_message(self.mess_id, f"Выберите одну из цифр")
+            self.sending_message.send_message(self.mess_id, f"1 - женский 2 - мужской")
+            receive_message = vk.Communication(token_group).listen()
+            search_term["sex"] = int(receive_message[1])
+        if search_term["age_to"] is None:
+            self.sending_message.send_message(self.mess_id, f"Укажите город для поиска")
+            self.sending_message.send_message(self.mess_id, f"Введите название города")
+            receive_message = vk.Communication(token_group).listen()
+            search_term["city"] = receive_message[1]
+        return search_term
+
+    def selection_candidates(self, arg, arg_1):
+        self.sending_message.send_message(self.mess_id, f"идет поиск кандидатов")
+        user_search = self.admin_group.user_search(arg, arg_1)
+        user_top_photo = self.admin_group.top_photo(user_search)
+        return user_top_photo
+
+    def show_photo(self, arg):
+        for i in arg:
+            uid, url = i.items()
+            photo_url = list(url[1][0].items())
+            show_can = self.admin_group.users_id(uid[0])
+            first_name = show_can[0].get("first_name")
+            last_name = show_can[0].get("last_name")
+            self.sending_message.send_message(self.mess_id, f"{first_name} {last_name}")
+            self.sending_message.send_message_media(self.mess_id, f"photo{uid[0]}_{photo_url[0][0]}")
+            time.sleep(1)
+        self.sending_message.send_message(self.mess_id, f"поиск окончен")
+
+
+def main():
+    con = db.create_database("db_vk")
+    vk_bot = VkGroup(my_login, my_password, token_group)
+    keyboard = VkKeyboard(one_time=False)
+    keyboard.add_button('поиск кандидатов', color=VkKeyboardColor.SECONDARY)
+    while True:
+        elimination_id = db.select_user_id(con)
+        vk_bot.sending_message.send_message(vk_bot.mess_id,
+                                            "нажмите на поиск кандидатов ", keyboard=keyboard.get_keyboard())
+
+        if vk_bot.mess_text() == "поиск кандидатов":
+            search_term = vk_bot.data_checking()
+            user_top_photo = vk_bot.selection_candidates(search_term, elimination_id)
+            db.insert_data(con, user_top_photo)
+            vk_bot.show_photo(user_top_photo)
+        else:
+            vk_bot.sending_message.send_message(vk_bot.mess_id, f"Я умею только искать, так что жми кнопку поиск")
+
+
+if __name__ == '__main__':
+    main()
